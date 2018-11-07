d6a114234314d6b3820d60de15d66ff6bd41afff
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-07 10:38:03 +0100

    tests

---
+++ src/test/java/CollectorsJoiningTest.java
@@ -0,0 +1,53 @@
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by mtumilowicz on 2018-11-07.
+ */
+public class CollectorsJoiningTest {
+    @Test
+    public void collectors_joining_default() {
+        var concat = Stream.of("a", "b", "c")
+                .collect(Collectors.joining());
+        
+        assertThat(concat, is("abc"));
+    }
+
+    @Test
+    public void collectors_joining_delimiter() {
+        var concat = Stream.of("a", "b", "c")
+                .collect(Collectors.joining(","));
+
+        assertThat(concat, is("a,b,c"));
+    }
+
+    @Test
+    public void collectors_joining_delimiter_prefix_suffix() {
+        var concat = Stream.of("a", "b", "c")
+                .collect(Collectors.joining(",",
+                        "prefix",
+                        "suffix"));
+
+        assertThat(concat, is("prefixa,b,csuffix"));
+    }
+    
+    @Test
+    public void string_join_args() {
+        var join = String.join(",", "a", "b", "c");
+    
+        assertThat(join, is("a,b,c"));
+    }
+
+    @Test
+    public void string_join_iterable() {
+        var join = String.join(",", List.of("a", "b", "c"));
+
+        assertThat(join, is("a,b,c"));
+    }
+}
